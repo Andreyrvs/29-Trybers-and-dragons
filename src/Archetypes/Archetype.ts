adcf8eadd9ad2700645deecbd7cbeac8d6d27486
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-18 00:32:59 -0300

    chore: Req 04 (Archetypes) - Adiciona Classe Archetype com atributos: name, special, cost. Metodo statico createdArchetypeInstances e um getter abstrato eneryType

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,32 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  constructor(
+    private _name: string,
+    private _special: number,
+    private _cost: number,
+  ) {
+    this._cost = 0;
+    this._special = 0;
+  }
+  
+  public get name() : string {
+    return this._name;
+  }
+  
+  public get special() : number {
+    return this._special;
+  }
+  
+  public get cost() : number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  public abstract get energyType() : EnergyType;
+}
+
+export default Archetype;
