24f990387660f0c464d867881580db53c8fb05bc
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-18 00:47:32 -0300

    feat: Req 05 (Archetypes) - Adiciona um class base que extende a classe Archetype, para todos esses arquivos

--- src/Archetypes/Ranger.ts
+++ src/Archetypes/Ranger.ts
@@ -2,12 +2,25 @@ import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 class Base extends Archetype {
+  private static _createdArchetypeInstances = 0;
+  private _energyType: EnergyType;
   constructor(name: string) {
     super(name);
+
+    this._energyType = 'mana';
+    Base.ArchetypeInstances();
   }
 
   public get energyType(): EnergyType {
-    throw new Error('Method not implemented.');
+    return this._energyType;
+  }
+
+  private static ArchetypeInstances() {
+    this._createdArchetypeInstances += 1;
+  }
+
+  static createdRacesInstances() {
+    return this._createdArchetypeInstances;
   }
 }
 
